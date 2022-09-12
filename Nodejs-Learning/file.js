d8872de1bf0b4ccd33702952ac42596b1c6bf7ef
Akhilchary <akhilp@nfcsolutionsusa.com>
2022-09-13 00:11:51 +0530

    day30

---
+++ Nodejs-Learning/file.js
@@ -0,0 +1,16 @@
+const l=["name1","name2","name3"];
+const age=[12,34,35,46,65]
+// module.exports=l
+// module.exports={
+//     l,age
+// }
+
+// or 
+// module.exports={
+//     l:l,age:age
+// }
+
+// or
+module.exports={
+    names:l,ages:age
+}
---
+++ Nodejs-Learning/server.js
@@ -0,0 +1,23 @@
+// const g=(name)=>{
+//     console.log(`hello,${name}`);
+// }
+// g("noob");
+// console.log(global); 
+
+// const lis=require("./file");
+
+// for(let i=0;i<lis.length;i++)
+// {
+//     console.log(lis[i]);
+// }
+
+// console.log(lis);
+
+//destructuring
+// const {names}=require("./file");
+// console.log(names);//here the destructring variable name 
+//                     //should match the name that is used 
+//                     //to export
+
+const os=require('os');
+console.log(os.platform());
